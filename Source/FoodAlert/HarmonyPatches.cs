c9511e510c87ed59e8741b3909e3785e825d5eee
Mehni <Mehni@users.noreply.github.com>
2018-05-16 17:53:35 +0200

    v0.18.0.2

---
+++ Source/FoodAlert/HarmonyPatches.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace FoodAlert
+{
+    [StaticConstructorOnStartup]
+    class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("Mehni.RimWorld.FoodAlert.Main");
+
+            harmony.Patch(AccessTools.Method(typeof(GlobalControlsUtility), nameof(GlobalControlsUtility.DoDate)), null,
+                new HarmonyMethod(typeof(HarmonyPatches), nameof(FoodCounter_NearDatePostfix)), null);
+        }
+
+        private static void FoodCounter_NearDatePostfix(float leftX, float width, ref float curBaseY)
+        {
+            {
+                var map = Find.VisibleMap;
+                if (map == null || !map.IsPlayerHome) return;
+                if (Find.TickManager.TicksGame < 150000) return;
+
+                float totalHumanEdibleNutrition = map.resourceCounter.TotalHumanEdibleNutrition;
+
+                if (totalHumanEdibleNutrition < 4f * (float)map.mapPawns.FreeColonistsSpawnedCount) return;
+
+                int humansGettingFood = map.mapPawns.FreeColonistsSpawnedCount + (from pr in map.mapPawns.PrisonersOfColony
+                                                                    where pr.guest.GetsFood
+                                                                    select pr).Count<Pawn>();
+
+                int totalDaysOfFood = Mathf.FloorToInt(totalHumanEdibleNutrition / humansGettingFood);
+                string daysWorthOfHumanFood = $"{totalDaysOfFood}" + "FoodAlert_DaysOfFood".Translate();
+                string addendumForFlavour = string.Empty;
+
+                switch (totalDaysOfFood)
+                {
+                    case int n when (n >= 100):
+                        addendumForFlavour = "FoodAlert_Ridiculous".Translate();
+                        break;
+
+                    case int n when (n >= 60):
+                        addendumForFlavour = "FoodAlert_Solid".Translate();
+                        break;
+
+                    case int n when (n >= 30):
+                        addendumForFlavour = "FoodAlert_Bunch".Translate();
+                        break;
+
+                    case int n when (n >= 10):
+                        addendumForFlavour = "FoodAlert_Decent".Translate();
+                        break;
+
+                    default:
+                        addendumForFlavour = "FoodAlert_Decent".Translate();
+                        break;
+                }
+
+                float rightMargin = 7f;
+                Rect zlRect = new Rect(UI.screenWidth - Alert.Width, curBaseY - 24f, Alert.Width, 24f);
+                Text.Font = GameFont.Small;
+
+                if (Mouse.IsOver(zlRect))
+                {
+                    Widgets.DrawHighlight(zlRect);
+                }
+
+                GUI.BeginGroup(zlRect);
+                Text.Anchor = TextAnchor.UpperRight;
+                Rect rect = zlRect.AtZero();
+                rect.xMax -= rightMargin;
+                if (Event.current.capsLock)
+                {
+                    rect.yMin -= 50f;
+                }
+                Widgets.Label(rect, daysWorthOfHumanFood);
+                Text.Anchor = TextAnchor.UpperLeft;
+                GUI.EndGroup();
+
+                TooltipHandler.TipRegion(zlRect, new TipSignal(delegate
+                {
+
+                    return string.Format("SomeFoodDesc".Translate(), totalHumanEdibleNutrition.ToString("F0"), humansGettingFood.ToStringCached(), totalDaysOfFood.ToStringCached() + addendumForFlavour);
+                }, 76515));
+
+                curBaseY -= zlRect.height;
+            }
+        }
+    }
+}
---
+++ Source/FoodAlert/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FoodAlert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FoodAlert")]
+[assembly: AssemblyCopyright("Copyright © Mehni 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f0e51d5-7f89-48ff-80e7-26f0d33c3b64")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.18.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
