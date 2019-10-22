f4450117529d3f9f0b8a77e631a503d16cccd031
Mehni <Mehni@users.noreply.github.com>
2019-10-22 22:15:07 +0200

    update for supportedVersions and fix for prisoner count

--- Source/FoodAlert/HarmonyPatches.cs
+++ Source/FoodAlert/HarmonyPatches.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Harmony;
+﻿using Harmony;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -20,76 +16,77 @@ namespace FoodAlert
                 new HarmonyMethod(typeof(HarmonyPatches), nameof(FoodCounter_NearDatePostfix)), null);
         }
 
-        private static void FoodCounter_NearDatePostfix(float leftX, float width, ref float curBaseY)
+        private static void FoodCounter_NearDatePostfix(ref float curBaseY)
         {
+            Map map = Find.CurrentMap;
+
+            if (map == null || !map.IsPlayerHome)
+                return;
+
+            if (Find.TickManager.TicksGame < 150000)
+                return;
+
+            float totalHumanEdibleNutrition = map.resourceCounter.TotalHumanEdibleNutrition;
+
+            if (totalHumanEdibleNutrition < 4f * map.mapPawns.FreeColonistsSpawnedCount)
+                return;
+
+            int humansGettingFood = map.mapPawns.FreeColonistsSpawnedCount + map.mapPawns.PrisonersOfColonySpawnedCount;
+
+            int totalDaysOfFood = Mathf.FloorToInt(totalHumanEdibleNutrition / humansGettingFood);
+            string daysWorthOfHumanFood = $"{totalDaysOfFood}" + "FoodAlert_DaysOfFood".Translate();
+            string addendumForFlavour;
+
+            switch (totalDaysOfFood)
             {
-                var map = Find.CurrentMap;
+                case int n when n >= 100:
+                    addendumForFlavour = "FoodAlert_Ridiculous".Translate();
+                    break;
 
-                if (map == null || !map.IsPlayerHome)
-                    return;
-                if (Find.TickManager.TicksGame < 150000)
-                    return;
+                case int n when n >= 60:
+                    addendumForFlavour = "FoodAlert_Solid".Translate();
+                    break;
 
-                float totalHumanEdibleNutrition = map.resourceCounter.TotalHumanEdibleNutrition;
+                case int n when n >= 30:
+                    addendumForFlavour = "FoodAlert_Bunch".Translate();
+                    break;
 
-                if (totalHumanEdibleNutrition < 4f * map.mapPawns.FreeColonistsSpawnedCount)
-                    return;
+                case int n when n >= 10:
+                    addendumForFlavour = "FoodAlert_Decent".Translate();
+                    break;
 
-                int humansGettingFood = map.mapPawns.FreeColonistsSpawnedCount + map.mapPawns.PrisonersOfColony.Count();
-
-                int totalDaysOfFood = Mathf.FloorToInt(totalHumanEdibleNutrition / humansGettingFood);
-                string daysWorthOfHumanFood = $"{totalDaysOfFood}" + "FoodAlert_DaysOfFood".Translate();
-                string addendumForFlavour = string.Empty;
-
-                switch (totalDaysOfFood)
-                {
-                    case int n when (n >= 100):
-                        addendumForFlavour = "FoodAlert_Ridiculous".Translate();
-                        break;
-
-                    case int n when (n >= 60):
-                        addendumForFlavour = "FoodAlert_Solid".Translate();
-                        break;
-
-                    case int n when (n >= 30):
-                        addendumForFlavour = "FoodAlert_Bunch".Translate();
-                        break;
-
-                    case int n when (n >= 10):
-                        addendumForFlavour = "FoodAlert_Decent".Translate();
-                        break;
-
-                    default:
-                        addendumForFlavour = "FoodAlert_Decent".Translate();
-                        break;
-                }
-
-                if (humansGettingFood == 0)
-                    addendumForFlavour = "\n\nShit you made me divide by zero. Disregard that.";
-
-                float rightMargin = 7f;
-                Rect zlRect = new Rect(UI.screenWidth - Alert.Width, curBaseY - 24f, Alert.Width, 24f);
-                Text.Font = GameFont.Small;
-
-                if (Mouse.IsOver(zlRect))
-                    Widgets.DrawHighlight(zlRect);
-
-                GUI.BeginGroup(zlRect);
-                Text.Anchor = TextAnchor.UpperRight;
-                Rect rect = zlRect.AtZero();
-                rect.xMax -= rightMargin;
-
-                Widgets.Label(rect, daysWorthOfHumanFood);
-                Text.Anchor = TextAnchor.UpperLeft;
-                GUI.EndGroup();
-
-                TooltipHandler.TipRegion(zlRect, new TipSignal(delegate
-                {
-                    return string.Format("SomeFoodDesc".Translate(), totalHumanEdibleNutrition.ToString("F0"), humansGettingFood.ToStringCached(), totalDaysOfFood.ToStringCached() + addendumForFlavour);
-                }, 76515));
-
-                curBaseY -= zlRect.height;
+                default:
+                    addendumForFlavour = "FoodAlert_Decent".Translate();
+                    break;
             }
+
+            if (humansGettingFood == 0)
+                addendumForFlavour = "\n\nShit you made me divide by zero. Disregard that.";
+
+            float rightMargin = 7f;
+            Rect zlRect = new Rect(UI.screenWidth - Alert.Width, curBaseY - 24f, Alert.Width, 24f);
+            Text.Font = GameFont.Small;
+
+            if (Mouse.IsOver(zlRect))
+                Widgets.DrawHighlight(zlRect);
+
+            GUI.BeginGroup(zlRect);
+            Text.Anchor = TextAnchor.UpperRight;
+            Rect rect = zlRect.AtZero();
+            rect.xMax -= rightMargin;
+
+            Widgets.Label(rect, daysWorthOfHumanFood);
+            Text.Anchor = TextAnchor.UpperLeft;
+            GUI.EndGroup();
+
+            TooltipHandler.TipRegion(zlRect, new TipSignal(
+                                                   () => string.Format("SomeFoodDesc".Translate(),
+                                                                       totalHumanEdibleNutrition.ToString("F0"),
+                                                                       humansGettingFood.ToStringCached(),
+                                                                       totalDaysOfFood.ToStringCached() + addendumForFlavour),
+                                                   76515));
+
+            curBaseY -= zlRect.height;
         }
     }
 }
